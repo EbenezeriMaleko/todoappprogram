6c731cf846722a982b937c105a613798085cface
EbenezeriMaleko <cebendemalek@gmail.com>
2025-02-28 15:08:10 +0300

    Intermediate modification[static view]

--- lib/widget/task_card.dart
+++ lib/widget/task_card.dart
@@ -9,7 +9,31 @@ class ToDoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile() ,
+      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListTile(
+          leading: Checkbox(
+            value: isCompleted, 
+            onChanged: (value){
+              //functions
+            },
+            activeColor: Colors.yellow,
+          ),
+        
+          title: Text(
+            task,
+            style: TextStyle(
+              fontSize: 14,
+              decoration: isCompleted ? TextDecoration.lineThrough : null,
+              color: isCompleted ? Colors.grey : null
+            ),
+          ),
+          trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
+        ),
+      ) 
     );
   }
 }
+   
